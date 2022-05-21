66f78141d514c43b96ad85fb6fa6d6c74e802aa3
Eric Christensen <hebrew.8.12@gmail.com>
2022-05-20 17:20:36 -0700

    v0.8.6

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,22 +1,20 @@
-let theme = localStorage.getItem("theme");
-let themeSwitcherInput = document.querySelector("#themeSwitch");
+let theme = localStorage.getItem('theme');
+let themeSwitcherInput = document.querySelector('#themeSwitch');
 
-if (theme === "light") {
-  console.log("light");
-  document.documentElement.setAttribute("data-theme", "light");
+if (theme === 'light') {
+  document.documentElement.setAttribute('data-theme', 'light');
   themeSwitcherInput.checked = false;
 } else {
-  console.log("dark");
-  document.documentElement.setAttribute("data-theme", "dark");
+  document.documentElement.setAttribute('data-theme', 'dark');
   themeSwitcherInput.checked = true;
 }
 
-themeSwitcherInput.addEventListener("change", (event) => {
+themeSwitcherInput.addEventListener('change', (event) => {
   if (event.target.checked) {
-    localStorage.setItem("theme", "dark");
-    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
   } else {
-    localStorage.setItem("theme", "light");
-    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
   }
 });
